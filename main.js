26ba3a84801219d54abfc076c5f8bd5337c07c00
rosette <irene153232@gmail.com>
2020-10-10 14:18:17 +0800

    填入main支線單字

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let port = process.env.PORT || 8888
 let Subject_router = require('./subject/router.js');
 
 app.get("/", function (request, response) {
-    response.send("高中職各科教學資訊");
+    response.send("高中職各科教學資訊(要到支線請填subject)");
 })
 app.use("/subject", Subject_router);
 app.listen(port, () => {
